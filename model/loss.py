0f1ed6e6bc68f8836cd5f809becb25a831856c33
Kirill Bubenchikov <63867291+bcd8697@users.noreply.github.com>
2022-06-29 18:19:29 +0300

    fixed lambda

--- model/loss.py
+++ model/loss.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-def sigmoid_cross_entropy_loss(logits, labels, lambda = 0.1, model, img_size):
+def sigmoid_cross_entropy_loss(logits, labels, l = 0.1, model, img_size):
   '''
   Function to compute loss according to the formula L = L_f + lambda * sum(L_i)
   For reference: https://arxiv.org/pdf/1708.04943.pdf
   ----------
   logits - logits (images before sigmoid/softmax convertation)
   labels - mask elements
-  lambda - empirical coefficient, non-negative parameter that leverages the trade-off between two losses (L_f and L_i).
+  l - empirical coefficient, non-negative parameter that leverages the trade-off between two losses (L_f and L_i).
   model - object of NN class
   img_size - tuple or list with (height, with) of images for training
   ----------
@@ -28,7 +28,7 @@ def sigmoid_cross_entropy_loss(logits, labels, lambda = 0.1, model, img_size):
                                                                             size = (img_size[0], img_size[1]), 
                                                                             method = 'bilinear'))
     
-  loss += lambda * L_i 
+  loss += l * L_i 
                                                                 
   # averaging across the batch
   return tf.reduce_mean(loss)
