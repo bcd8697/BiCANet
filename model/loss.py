f4d70f409115a18771e3445b347f815d6e437395
Kirill Bubenchikov <63867291+bcd8697@users.noreply.github.com>
2022-06-30 01:12:04 +0300

    added mod_seq class

--- model/loss.py
+++ model/loss.py
@@ -1,6 +1,25 @@
 import tensorflow as tf
+from tensorflow.keras import Model, layers
 
-def sigmoid_cross_entropy_loss(logits, labels, model, img_size, l = 0.1):
+class mod_seq(Model):
+  '''
+  Sequence for the complex loss to calculate the result in the end of every CCPB Block
+  ----------
+  layer_bb - layer from the backbone architecture before CCPB block
+  layer_ccpb - CCPB block which goes after the corresponding backbone layer
+  ----------
+  '''
+  
+  def __init__(self, layer_bb, layer_ccpb):
+    super(mod_seq, self).__init__()
+    
+    self.layer_bb = layer_bb
+    self.layer_ccpb = layer_ccpb
+
+  def call(self, x):
+    return self.layer_ccpb(self.layer_bb(x))
+
+def sigmoid_cross_entropy_loss(logits, labels, images, model, img_size, l = 0.1):
   '''
   Function to compute loss according to the formula L = L_f + lambda * sum(L_i)
   For reference: https://arxiv.org/pdf/1708.04943.pdf
@@ -19,12 +38,12 @@ def sigmoid_cross_entropy_loss(logits, labels, model, img_size, l = 0.1):
   
   # compute losses L_i
   for i in range(4):
-    mod_seq = tf.keras.models.Sequential([tf.keras.models.Model(inputs = model.layers[0].inputs, 
-                                                                outputs = model.layers[0].get_layer('block' + str(i+1) + '_pool').output),
-                                          model.layers[i+1] ])
+    m = mod_seq(layer_bb = tf.keras.models.Model(inputs = model.layers[0].inputs,
+                                                   outputs = model.layers[0].get_layer('block' + str(i+1) + '_pool').output),
+                  layer_ccpb = model.layers[i+1])(images)
     
     L_i += tf.nn.sigmoid_cross_entropy_with_logits(labels = labels,
-                                                   logits = tf.image.resize(mod_seq.output, 
+                                                   logits = tf.image.resize(m, 
                                                                             size = (img_size[0], img_size[1]), 
                                                                             method = 'bilinear'))
     
